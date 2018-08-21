3ba80ff2edb879d11a15a3163a1d58954a02c1f8
Brad Hankee <brad.hankee@gmail.com>
2018-08-20 20:20:50 -0400

    Set up express server and graph ql schema.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+var wods = [
+  { name: 'foreach', difficulty: 'medium', id: '1' },
+  { name: 'pop', difficulty: 'easy', id: '2' },
+  { name: '.random', difficulty: 'hard', id: '3' }
+];
 
 const WodType = new GraphQLObjectType({
   name: 'Wod',
@@ -9,4 +16,22 @@ const WodType = new GraphQLObjectType({
     name: { type: GraphQLString },
     difficulty: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    wod: {
+      type: WodType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from DB
+        return _find(wods, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });

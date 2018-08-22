8ce02fd0672df3b62179eb9112ec4891e506db43
Brad Hankee <brad.hankee@gmail.com>
2018-08-21 20:25:02 -0400

    Set up mongoDB and Mongoose.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({ schema }));
+mongoose.connect(
+  `mongodb://bhankee:${process.env.MONGO_DB}@ds227352.mlab.com:27352/workoutjs`
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database!');
+});
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(8000, () => {
-  console.log('Listening for requests on PORT 4000!');
+  console.log('Listening for requests on PORT 8000!');
 });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,18 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
-
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList
+} = graphql;
+var groups = [{ name: 'Array', id: '1' }, { name: 'String', id: '2' }];
 var wods = [
-  { name: 'foreach', difficulty: 'medium', id: '1' },
-  { name: 'pop', difficulty: 'easy', id: '2' },
-  { name: '.random', difficulty: 'hard', id: '3' }
+  { name: 'foreach', difficulty: 'medium', id: '1', groupId: '1' },
+  { name: 'pop', difficulty: 'easy', id: '2', groupId: '1' },
+  { name: '.random', difficulty: 'hard', id: '3', groupId: '2' }
 ];
 
 const WodType = new GraphQLObjectType({
@@ -14,7 +20,28 @@ const WodType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    difficulty: { type: GraphQLString }
+    difficulty: { type: GraphQLString },
+    group: {
+      type: GroupType,
+      resolve(parent, args) {
+        console.log('PARENT: ', parent);
+        return _.find(groups, { id: parent.groupId });
+      }
+    }
+  })
+});
+
+const GroupType = new GraphQLObjectType({
+  name: 'Group',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    wods: {
+      type: new GraphQLList(WodType),
+      resolve(parent, args) {
+        return _.filter(wods, { groupId: parent.id });
+      }
+    }
   })
 });
 
@@ -23,10 +50,29 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     wod: {
       type: WodType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from DB
-        return _find(wods, { id: args.id });
+        return _.find(wods, { id: args.id });
+      }
+    },
+    group: {
+      type: GroupType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(groups, { id: args.id });
+      }
+    },
+    wods: {
+      type: new GraphQLList(WodType),
+      resolve(parent, args) {
+        return wods;
+      }
+    },
+    groups: {
+      type: new GraphQLList(GroupType),
+      resolve(parent, args) {
+        return groups;
       }
     }
   }

492f0ed7affad51265d2efd3ee3d5776ce14748a
Brad Hankee <brad.hankee@gmail.com>
2018-09-04 16:59:25 -0400

    Added timing logic to push component.

--- client/src/components/Push.js
+++ client/src/components/Push.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 
+import './Push.css';
+
 class Push extends Component {
-  render() {
+  state = {
+    wods: [],
+    itemsRendered: 0
+  };
+
+  startWod = () => {
+    this.pushWod();
+  };
+
+  pushWod = () => {
+    this.timer = setTimeout(this.updateRenderedThings, 1000);
+  };
+
+  updateRenderedThings = () => {
+    console.log('UPDATING');
+    const { itemsRendered, wods } = this.state;
     const { movements } = this.props;
+    const updatedState = {
+      wods: wods.concat(movements[itemsRendered]),
+      itemsRendered: itemsRendered + 1
+    };
+
+    this.setState(updatedState);
+    console.log('WOD STATE: ', wods);
+    if (updatedState.itemsRendered < movements.length) {
+      this.pushWod();
+    } else {
+      clearTimeout(this.timer);
+    }
+  };
+
+  render() {
+    const { wods } = this.state;
+    const { movements } = this.props;
+
     return (
       <div>
-        <h1>[{movements}]</h1>
+        [
+        {wods.map((thing, index) => (
+          <div key={index}>{thing}</div>
+        ))}
+        ]<button onClick={this.startWod}>Start Workout</button>
       </div>
     );
   }

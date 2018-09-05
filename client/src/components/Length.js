53a523717aa62afbb16fda0876b0bfd89e411b9e
Brad Hankee <brad.hankee@gmail.com>
2018-09-05 11:31:01 -0400

    Added length proprty in dom work each move.

--- client/src/components/Length.js
+++ client/src/components/Length.js
@@ -6,27 +6,34 @@ import './Push.css';
 class Length extends Component {
   state = {
     wods: [],
-    itemsRendered: 0
+    itemsRendered: 0,
+    cuurentString: '',
+    currentLength: null
   };
 
   startWod = () => {
     this.pushWod();
+    console.log('MOVEMENTS:', this.props.movements);
   };
 
   pushWod = () => {
-    this.timer = setTimeout(this.updateRenderedThings, 1000);
+    this.timer = setTimeout(this.updateRenderedThings, 5000);
   };
 
   updateRenderedThings = () => {
     console.log('UPDATING');
-    const { itemsRendered, wods } = this.state;
+    const { itemsRendered, wods, currentString, currentLength } = this.state;
     const { movements } = this.props;
     const updatedState = {
       wods: wods.concat(movements[itemsRendered]),
+      currentString: movements[itemsRendered],
+      currentLength: movements[itemsRendered].toString(),
       itemsRendered: itemsRendered + 1
     };
 
     this.setState(updatedState);
+    console.log('CURRENT MOVE: ', currentString);
+    console.log('CURRENT Length: ', currentLength);
     console.log('WOD STATE: ', wods);
     if (updatedState.itemsRendered < movements.length) {
       this.pushWod();
@@ -35,17 +42,23 @@ class Length extends Component {
     }
   };
 
-  render() {
+  current = () => {
     const { wods } = this.state;
+    wods.map((move, index) => {
+      this.setState({ currentString: move });
+    });
+  };
+
+  render() {
+    const { wods, currentString, currentLength } = this.state;
     const { movements } = this.props;
 
     return (
       <div className="pushWrapper">
-        [
-        {wods.map((move, index) => (
-          <li key={index}>{move} ,</li>
-        ))}
-        ]<Timer run={this.startWod} />
+        <h2>{currentString}</h2>
+        <h2>{currentLength ? currentLength.length : null}</h2>
+
+        <Timer run={this.startWod} />
       </div>
     );
   }

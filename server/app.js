3a5897b3ff59de36452afca4adea2ea1c385bc64
Brad Hankee <brad.hankee@gmail.com>
2018-09-13 16:57:35 -0400

    Added engin node versin in package.json.

--- server/app.js
+++ server/app.js
@@ -4,15 +4,16 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 console.log('INSIDE APP. JS -------------------------------');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 //allow cross-origin requests
 app.use(cors());
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 mongoose.connect(
   `mongodb://bhankee:${process.env.MONGO_DB}@ds227352.mlab.com:27352/workoutjs`
@@ -24,5 +25,5 @@ mongoose.connection.once('open', () => {
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(PORT, () => {
-  console.log('Listening for requests on PORT 5000!');
+  console.log('Listening for requests on PORT 8000!');
 });

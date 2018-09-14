f1a3fbdded3fe17001f7d9eae3666e8b495d218f
Brad Hankee <brad.hankee@gmail.com>
2018-09-13 21:33:46 -0400

    Added engin node versin in package.json.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+const schema = require('./server/schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
@@ -19,6 +19,13 @@ mongoose.connect(
 );
 mongoose.connection.once('open', () => {
   console.log('connected to database!');
+});
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
